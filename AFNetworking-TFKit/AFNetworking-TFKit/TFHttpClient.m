42f40a593528702ac7e7755604f9696b3fdc3a70
TwistedFate <zhangpeng@zhilunkeji.com>
2020-05-25 14:37:02 +0800

    添加httpClient代码

--- AFNetworking-TFKit/AFNetworking-TFKit/TFHttpClient.m
+++ AFNetworking-TFKit/AFNetworking-TFKit/TFHttpClient.m
@@ -12,16 +12,14 @@
 #import "AFNetworking-TFKit.h"
 
 @interface TFHttpClient ()
-
 @property (nonatomic, strong) AFHTTPSessionManager *manager;
-
 @end
 
 @implementation TFHttpClient
 
 - (instancetype)initWithBaseURL:(NSURL *)baseURL {
     if (self = [super init]) {
-        _manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@""]];
+        _manager = [[AFHTTPSessionManager alloc] initWithBaseURL:baseURL];
         _manager.requestSerializer = [AFJSONRequestSerializer serializer];
         _manager.responseSerializer = [AFJSONResponseSerializer serializer];
         [_manager.requestSerializer setTimeoutInterval:10];
@@ -29,13 +27,36 @@
     return self;
 }
 
-+ (TFHttpClient *)client {
-    TFHttpClient *client = [[[self class] alloc ] init];
++ (instancetype)loginClient {
+    TFHttpClient *client = [[self alloc] initWithBaseURL:[NSURL URLWithString:@""]];
     return client;
 }
 
+/// GET
+- (void)GET:(NSString *)URLString parameters:(nonnull id)parameters success:(nullable TFResponseBlock)success failure:(nullable TFResponseBlock)failure {
+    [self.manager GET:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask *_Nonnull task, id _Nullable responseObject) {
+        TFResponse *response = [self handleTask:task responseObject:responseObject error:nil];
+        BLOCK_EXEC(success, response);
+    } failure:^(NSURLSessionDataTask *_Nullable task, NSError *_Nonnull error) {
+        TFResponse *response = [self handleTask:task responseObject:nil error:error];
+        BLOCK_EXEC(failure, response);
+    }];
+}
+
+- (void)POST:(nonnull NSString *)URLString parameters:(nullable id)parameters progress:(nullable void (^)(NSProgress * _Nonnull))uploadProgress success:(nullable TFResponseBlock)success failure:(nullable TFResponseBlock)failure {
+    
+    [self.manager POST:URLString parameters:parameters progress:uploadProgress success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        TFResponse *response = [self handleTask:task responseObject:responseObject error:nil];
+        BLOCK_EXEC(success, response);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        TFResponse *response = [self handleTask:task responseObject:nil error:error];
+        BLOCK_EXEC(failure, response);
+    }];
+}
+
 /// 请求结果处理
-- (TFResponse *)handleTask:(NSURLSessionDataTask *)task responseObject:(nullable id)responseObject error:(NSError *)error {
+- (TFResponse *)handleTask:(NSURLSessionDataTask *)task responseObject:(id)responseObject error:(NSError *)error {
+    
     TFResponse *response = [[TFResponse alloc] init];
     if (error) {
         response.success = NO;
@@ -59,23 +80,11 @@
         }
     } else {
         response.success = YES;
+        /// 请求成功
         response.statusCode = [responseObject integerForKey:@"statusCode"];
         response.data = responseObject[@"data"];
     }
     return response;
 }
-
-
-/// GET
-- (void)GET:(NSString *)URLString parameters:(nonnull id)parameters success:(nullable TFResponseBlock)success failure:(nullable TFResponseBlock)failure {
-    [self.manager GET:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask *_Nonnull task, id _Nullable responseObject) {
-        TFResponse *response = [self handleTask:task responseObject:responseObject error:nil];
-        BLOCK_EXEC(success, response);
-    } failure:^(NSURLSessionDataTask *_Nullable task, NSError *_Nonnull error) {
-        TFResponse *response = [self handleTask:task responseObject:nil error:error];
-        BLOCK_EXEC(failure, response);
-    }];
-}
-
 
 @end
